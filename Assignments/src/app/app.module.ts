ad332fcd47376e78e5241f238f5c8b12049d0b91
manerajeshri <manerajeshri@gmail.com>
2023-02-09 04:15:59 +0530

    update
    update

--- Assignments/src/app/app.module.ts
+++ Assignments/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildToParentComponent } from './child-to-parent/child-to-parent.component';
 import { FormsModule } from '@angular/forms';
+import { CountryStateComponent } from './country-state/country-state.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildToParentComponent
+    ChildToParentComponent,
+    CountryStateComponent
+   
   ],
   imports: [
     BrowserModule,
