2587525d09cf8482650bf282da3ba4732dadf4ee
manerajeshri <manerajeshri@gmail.com>
2023-02-14 08:32:08 +0530

    updated with ng modules

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -11,6 +11,39 @@ import { FormsModule } from '@angular/forms';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { CustomDirective } from './custom.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// getting from angular material toggle button 
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// adding from angular material : slider
+// import {MatSliderModule} from '@angular/material/slider';
+import { TemplateformComponent } from './templateform/templateform.component';
+// for form
+// import {MatCardModule} from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import {  MatInputModule } from "@angular/material/input";
+import {  MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import {  MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 @NgModule({
@@ -22,12 +55,44 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     NgIfComponent,
     NgSwitchComponent,
     NgStyleComponent,
-    NgClassComponent
+    NgClassComponent,
+    CustomDirective,
+    TemplateformComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule, // for toggle button
+    MatSliderModule, // // adding from angular material : slider
+    MatCardModule, // for form
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatProgressBarModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
