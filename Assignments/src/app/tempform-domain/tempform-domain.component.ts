8235b02cee42cf1a031d6ec712fa32bfb3fce57b
manerajeshri <manerajeshri@gmail.com>
2023-02-17 12:19:59 +0530

    Assignments 16 feb : validate domain
    Assignments 16 feb
    ● User can only able to insert ‘codemindtechnology.com’ domain
    ● Like dnyanesh@codemindtechnology.com
    ● If user trying to insert dnyanesh@test.com or dnyansh@gmail.com
    ● We need to display a message like please enter a valid domain email

---
+++ Assignments/src/app/tempform-domain/tempform-domain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tempform-domain',
+  templateUrl: './tempform-domain.component.html',
+  styleUrls: ['./tempform-domain.component.css']
+})
+export class TempformDomainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
