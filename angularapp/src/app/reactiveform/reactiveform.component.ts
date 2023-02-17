f722708a0a872c50d03e11ca6a1fe8de87e11139
manerajeshri <manerajeshri@gmail.com>
2023-02-17 20:22:22 +0530

    reactive form Learning
    reactive form Learning

---
+++ angularapp/src/app/reactiveform/reactiveform.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactiveform',
+  templateUrl: './reactiveform.component.html',
+  styleUrls: ['./reactiveform.component.css']
+})
+export class ReactiveformComponent implements OnInit {
+
+  // 1]create instace 
+  myReactiveForm : FormGroup; // declaring form group collection of form control is formGroup
+  constructor() { 
+    //3] calling method by default
+    this.createForm(); // called by default as constructor called by default
+  }
+
+  ngOnInit() {
+  }
+// 2] write method and call in constructor
+createForm(){
+  this.myReactiveForm= new FormGroup({
+    'username' : new FormControl (null),
+    'email': new FormControl(null)
+
+})
+}
+
+onSubmit(){
+  alert("Method called");
+
+  console.log(`My ReactiveForm`,this.myReactiveForm);
+  console.log(`My ReactiveForm`,this.myReactiveForm.value);
+  
+}
+
+}
