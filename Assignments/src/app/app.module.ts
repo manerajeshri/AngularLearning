481d9d96f0fa6a7f861fd8e71d1a3b0b4fcaf304
manerajeshri <manerajeshri@gmail.com>
2023-02-17 12:18:09 +0530

    update
    update

--- Assignments/src/app/app.module.ts
+++ Assignments/src/app/app.module.ts
@@ -8,6 +8,34 @@ import { FormsModule } from '@angular/forms';
 import { CountryStateComponent } from './country-state/country-state.component';
 import { DirectiveProjectComponent } from './directive-project/directive-project.component';
 import { DirectiveComponent } from './directive/directive.component';
+import { TemplateformComponent } from './templateform/templateform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//from angular material adding modules
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import {  MatInputModule } from "@angular/material/input";
+import {  MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import {  MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { TempformDomainComponent } from './tempform-domain/tempform-domain.component';
 
 
 @NgModule({
@@ -16,13 +44,46 @@ import { DirectiveComponent } from './directive/directive.component';
     ChildToParentComponent,
     CountryStateComponent,
     DirectiveProjectComponent,
-    DirectiveComponent
+    DirectiveComponent,
+    TemplateformComponent,
+    TempformDomainComponent,
+    // adding from angular material
+    
+    
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatProgressBarModule
+
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/templateform/templateform.component.ts
+++ angularapp/src/app/templateform/templateform.component.ts
@@ -8,18 +8,62 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateformComponent implements OnInit {
 
-  constructor() { }
+// for entered data display or not 
+isSubmitted : boolean =false;
 
+// for entered data display 
+formData  = {
+  email: '',
+  password: '',
+  course: '',
+  gender: ''
+}
+// setting default value to display when refresh or on start
+defaultValue="Angular";
+defaultGender = 'Male';
+
+// courses array
+courses: string[] = ['Angular', 'Javascript', 'Typescript'];
+
+// gender array
+genders = [ 
+  {
+    id: '1', value: 'Male'
+  },
+  {
+    id: '2', value: 'Female'
+  }
+]
+
+emails='';
+// isdisabled :boolean= false; not required
+
+constructor() { }
   ngOnInit() {
   }
 
-  emails='';
-
-  // isdisabled :boolean= false; not required
+  
   login(form : NgForm){
-    console.log(`form contents : form.values : `,form.value);
-    console.log(`is Form valid? : `,form.valid); // to get form is valid or not use .valid
+    this.isSubmitted= true;
+
+    // console.log(form);
+    // console.log(`form contents : form.values : `,form.value);
+    // console.log(`is Form valid? : `,form.valid); // to get form is valid or not use .valid
+
+    this.formData.email = form.value.userDetail.email;
+    this.formData.password = form.value.userDetail.password;
+    this.formData.course = form.value.course;
+    this.formData.gender = form.value.gender;
     
+    form.reset(); // to reset form
+
+    form.controls['course'].setValue('Angular'); // to set default values multiple values
+    form.controls['gender'].patchValue('Male'); // to set default values only one value
     
+    // POST api/ Saveuser(formData);
   }
+
+
+
 }
+
