a8c3245a13df2d677c00cfeba2d534701e6a40b9
manerajeshri <manerajeshri@gmail.com>
2023-02-24 23:50:46 +0530

    ngIF
    ngIF

---
+++ Assignments/src/app/ng-if/ng-if.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-if',
+  templateUrl: './ng-if.component.html',
+  styleUrls: ['./ng-if.component.css']
+})
+export class NgIfComponent implements OnInit {
+
+
+  // 
+  condition:boolean= true;
+  disable :boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
