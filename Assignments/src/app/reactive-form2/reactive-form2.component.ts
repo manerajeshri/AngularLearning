674e796e01569f302068a627a4eed161dd37d536
manerajeshri <manerajeshri@gmail.com>
2023-02-28 01:26:41 +0530

    updated format and added User details form group
    updated format and added User details form group

--- Assignments/src/app/reactive-form2/reactive-form2.component.ts
+++ Assignments/src/app/reactive-form2/reactive-form2.component.ts
@@ -11,17 +11,46 @@ export class ReactiveForm2Component implements OnInit {
   reactiveForm : FormGroup;
   createForm(){
     this.reactiveForm = new FormGroup({
-      'email' : new FormControl(null,[Validators.required,Validators.email]),
-      'subscription' : new FormControl("Advanced"),
-      'password' : new FormControl(null,Validators.required),
-      'date' :new FormControl(null),
+      // User Details formgroup
+      'userDetails': new FormGroup({
+        'fullName': new FormControl(null,Validators.required),
+        'yourBirthday': new FormControl(null,Validators.required),
+        'gender': new FormControl(null,Validators.required),
+        'country': new FormControl(null,Validators.required),
+        'phone': new FormControl(null,[Validators.required]),
+        'bio': new FormControl(null),
+      }),
+     
+
+      // Account Details formgroup
+
+      'accountDetails': new FormGroup({
+        'username': new FormControl(null),
+        'email' : new FormControl(null,[Validators.required,Validators.email]),
+        'password' : new FormControl(null,Validators.required),
+        'confirmPassword' : new FormControl(null,Validators.required),
+      })
+      
     })
   }
+
+  onSubmit(){
+  }
+  onClick(){
+    // logging all form data
+console.log(`All form details`, this.reactiveForm);
+console.log(`All form Value`, this.reactiveForm.value);
+console.log(`All form Value`, this.reactiveForm.value.fullName);
+
+  }
+
   constructor() { 
     this.createForm();
   }
 
   ngOnInit() {
+      
+
   }
 
 }
