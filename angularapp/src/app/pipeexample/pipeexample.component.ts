42966e541d6da97392fccaf52940215ff9316cf9
manerajeshri <manerajeshri@gmail.com>
2023-03-06 21:33:14 +0530

    pipe
    pipe

--- angularapp/src/app/pipeexample/pipeexample.component.ts
+++ angularapp/src/app/pipeexample/pipeexample.component.ts
@@ -6,14 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pipeexample.component.css']
 })
 export class PipeexampleComponent implements OnInit {
+  // taking new date
 todaysDate = new Date();
 price:number = 70000;
 employee = {
   empid : '1',
   empname : 'Jack'
 }
-
-countLength : number =500;
+// variable for custom pipe
+countLength : number = 500;
 
   constructor() { }
 
