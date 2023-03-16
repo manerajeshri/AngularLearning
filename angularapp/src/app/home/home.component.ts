691c4f664d9d0dfe43983d2aba6eff40a89ee624
manerajeshri <manerajeshri@gmail.com>
2023-03-16 07:57:36 +0530

    fetching key and data in array format
    fetching key and data in array format

--- angularapp/src/app/home/home.component.ts
+++ angularapp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
-import { map} from 'rxjs/operators'
+import { map,filter} from 'rxjs/operators'
+import { of,from } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,25 @@ export class HomeComponent implements OnInit {
   constructor(private firebaseService : FirebaseService) { }
 
   ngOnInit() {
-console.log(`receiveddata`,this.receiveddata);
+// console.log(`receiveddata`,this.receiveddata);
+
+// //emit (1,2,3,4,5)
+// const source= from([1,2,3,4,5]);
+
+// // filterout non even numbers
+
+// const example = source.pipe(filter(num => num%2 === 0));
+// example.subscribe(res => {
+//   console.log(`Even number :${res}`);
+// })
+
+
+// const obs = of(1,2,3).pipe(
+//   map(x => x+1 )
+// ).subscribe(res=> {
+//   console.log(`obs of`, res);
+// })
+
 
   }
   createPost(){
@@ -23,23 +42,51 @@ console.log(`receiveddata`,this.receiveddata);
   }
 
 receiveddata;
-getData(){
-    this.firebaseService.getPostDataFirebase()
-    .pipe(map(resData =>{
-      console.log(resData);
-      const dataArray=[];
-      for(const key in resData){
-      // console.log(key);
-      // console.log(resData[key]);
-      dataArray.push({id:key,...resData[key]})
-      }
-return dataArray
+// getData(){
+//     this.firebaseService.getPostDataFirebase()
+//     .pipe(map(resData =>{
+//       console.log(resData);
+//       const dataArray=[];
+//       for(const key in resData){
+//       // console.log(key);
+//       // console.log(resData[key]);
+//       dataArray.push({id:key,...resData[key]})
+//       }
+// return dataArray
 
-    }))
-    .subscribe(res=>{
-      console.log(`Firebase Get Data`, res);
-      this.receiveddata = res;
+//     }))
+//     .subscribe(res=>{
+//       console.log(`Firebase Get Data`, res);
+//       this.receiveddata = res;
+//     })
+  // }
+
+
+  getData(){
+    this.firebaseService.getPostDataFirebase().pipe(
+      map(responseData => {
+        console.log(`before manipulate data`, responseData);
+        
+        // empty array
+        const postArray = [];
+        // for in loop
+        for (const key in responseData) {
+          // check the keys
+          if (Object.prototype.hasOwnProperty.call(responseData, key)) {
+            const element = responseData[key];
+            // push new value into array
+            postArray.push({...responseData[key],id:key})
+            
+          }
+        }
+        return postArray
+      })
+    ).subscribe(res=>{
+      console.log(`aftermanipulate data`, res);
+      
     })
   }
+
+
 
 }
