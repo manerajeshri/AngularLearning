e8ebaed50701734613c0586389e8515bdf369b75
manerajeshri <manerajeshri@gmail.com>
2023-03-20 00:37:33 +0530

    Assignment wikipedia Search API - component creation
    Assignment wikipedia Search API - component creation

---
+++ Assignments/src/app/wikisearchapi/wikisearchapi.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { WikisearchapiService } from '../services/wikisearchapi.service';
+
+@Component({
+  selector: 'app-wikisearchapi',
+  templateUrl: './wikisearchapi.component.html',
+  styleUrls: ['./wikisearchapi.component.css']
+})
+export class WikisearchapiComponent implements OnInit {
+
+  // variable
+receivedData;
+
+  // 4] DI and import WikisearchapiService
+  constructor(private wikisearchapiService : WikisearchapiService) { }
+
+  ngOnInit() {
+
+    this.wikisearchapiService.getData1().subscribe(res=>{
+      console.log(`res`,res);
+this.receivedData=res;
+      
+    })
+
+
+    }
+  }
+
+
