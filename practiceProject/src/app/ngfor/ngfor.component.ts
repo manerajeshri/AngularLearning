e3ab7a71ca34f3268aca4167bb3822f81a7a3324
manerajeshri <manerajeshri@gmail.com>
2023-05-08 10:15:34 +0530

    ngFor
    ngFor

--- practiceProject/src/app/ngfor/ngfor.component.ts
+++ practiceProject/src/app/ngfor/ngfor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movies } from '../models/movies';
 
 @Component({
   selector: 'app-ngfor',
@@ -8,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class NgforComponent implements OnInit {
 //
 arrayOfNumbers=[1,2,3,'four', 'five'];
+
+// array of object using models
+movies : Movies[] = [
+  {title : 'kantara' ,  year : 2021},
+  {title : 'DDLJ' ,  year : 2022},
+  {title : 'Ved' ,  year : 2023},
+  {title : '&' ,  year : 2024},
+
+]
+
+
+
   constructor() { }
 
   ngOnInit() {
